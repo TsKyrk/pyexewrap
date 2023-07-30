aa8abffd729c35b4d490cd70b0e20237aa389249
Tansu KAYRAK <tansu.kayrak@gmail.com>
2023-07-31 00:26:52 +0200

    First commit

---
+++ example_scripts/02_HelloWorld_from_cmd.bat
@@ -0,0 +1,4 @@
+@echo off
+call 01_HelloWorld.py
+echo Notice that the script has not been paused by python.exe
+pause
---
+++ example_scripts/03_Import_Error.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python -m pyexewrap
+import a_package_you_dont_have
+print("hello world")
