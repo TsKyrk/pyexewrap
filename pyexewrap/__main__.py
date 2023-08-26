00f2f7646f2323f75b4a2bb295cdbedafa40352c
Tansu KAYRAK <tansu.kayrak.pro@gmail.com>
2023-08-26 14:28:28 +0200

    altered traceback issue solved

--- pyexewrap/__main__.py
+++ pyexewrap/__main__.py
@@ -3,6 +3,26 @@ import sys
 import traceback
 import code
 
+# Copied and adapted from Python IDLE's source code here : https://github.com/python/cpython/blob/3.11/Lib/code.py#L131
+# see https://github.com/python/cpython/blob/3.11/LICENSE
+# Maybe I could have somehow imported and used InteractiveInterpreter class from cpython/Lib/code.py instead ?
+def showtraceback():
+    """
+    Display the exception that just occurred. But first we remove the first stack item because it is our own code.
+    """
+    sys.last_type, sys.last_value, last_tb = ei = sys.exc_info()
+    sys.last_traceback = last_tb
+    try:
+        lines = traceback.format_exception(ei[0], ei[1], last_tb.tb_next)
+        if sys.excepthook is sys.__excepthook__:
+            print(''.join(lines))
+        else:
+            # If someone has set sys.excepthook, we let that take precedence
+            # over our print
+            sys.excepthook(ei[0], ei[1], last_tb)
+    finally:
+        last_tb = ei = None
+
 # Main function of the pyexewrap package
 def main():
     # pyexewrap_verbose = True # Uncomment to debug with verbose mode
@@ -26,13 +46,16 @@ def main():
 
             # Execute the script code within the current context
             # note that globals are also binded to exec's locals. This is mandatory : see the unitary test E001.
-            exec(script_code, globals(), globals())
+            code = compile(script_code,script_to_execute,"exec")
+            exec(code, globals(), globals())
 
             if pyexewrap_verbose: print("pyexewrap_mustpause_in_console="+str(pyexewrap_mustpause_in_console))
 
         except Exception as e:
-            print(f"Error executing {script_to_execute}: {type(e).__name__}")
-            print(traceback.format_exc())
+            # These 2 lines are now replaced by showtraceback():
+            # print(f"Error executing {script_to_execute}: {type(e).__name__}")
+            # print(traceback.format_exc())
+            showtraceback()
             print("This exception has ended the script before the end.")
 
     if pyexewrap_verbose: print("pyexewrap ended.")
