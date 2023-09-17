d807eaab8212f2e54695ddd205753c77ab2e4263
Tansu KAYRAK <tansu.kayrak.pro@gmail.com>
2023-09-17 03:05:45 +0200

    reverted the cherry-picking strategy for exec namespace (E001 was failing again : imports not working in exec()) changed code structure

--- pyexewrap/__main__.py
+++ pyexewrap/__main__.py
@@ -55,6 +55,7 @@ def display_pause_prompt_and_menu():
     """
     Pauses a script to let the user read stdout and/or strerr before the window gets closed
     """
+    # Looping since the pausing message can be repeated if needed
     while True:
         wait = None
         while wait is None:
@@ -62,6 +63,9 @@ def display_pause_prompt_and_menu():
                 wait = input("Press <Enter> to Quit. (<c> for cmd console. <i> for interactive python. <r> to restart.)\n")
             except:
                 pass  # Preventing unwanted Ctrl-C repetitions
+        
+        # By default, the script shall end after this prompt
+        must_run_script_again = False
         match wait.lower():
             case "c":
                 print('Opening a windows console (cmd.exe). Type "exit" to quit.\n\n')
@@ -83,7 +87,7 @@ def display_pause_prompt_and_menu():
                 print("\n")
             case "r":
                 os.system("cls")
-                main()
+                must_run_script_again = True
                 break
             case "debug":
                 # Secret feature to help developping new features
@@ -96,94 +100,102 @@ def display_pause_prompt_and_menu():
             case _:
                 # The commands must be typed accurately. Must retry...
                 wait = None
+        return must_run_script_again
 
-# Main function of the pyexewrap package
-def main():
+
+def run_script(script_to_execute):
     ################ BEHAVIOUR CUSTOMIZATION ######
+    global pyexewrap_must_pause_in_console
     pyexewrap_must_pause_in_console = True  # This global can be changed dynamicaly by the enhanced scripts
     pyexewrap_must_change_title = True
     pyexewrap_verbose = False
     # pyexewrap_verbose = True  # Uncomment to debug with verbose mode
 
-
     if pyexewrap_verbose: print("pyexewrap activated.")
 
-    if len(sys.argv) < 2: 
-        print("Usage: python wrap.py <script.py>")
-    else:
-        script_to_execute = sys.argv[1]
-        script_extension = os.path.splitext(script_to_execute)[1]
-        script_is_doubleclicked = ('PROMPT' not in os.environ) or ('pyexewrap_simulate_doubleclick' in os.environ)
+    script_extension = os.path.splitext(script_to_execute)[1]
+    script_is_doubleclicked = ('PROMPT' not in os.environ) or ('pyexewrap_simulate_doubleclick' in os.environ)
 
-        if "pythonw" in sys.executable:
-            err_msg = "Error : pyexewrap should never be running with pythonw.exe !\n" + str(sys.executable) + "\n" + str(sys.argv)
-            print(err_msg)
-            with open("error.txt", encoding="UTF-8") as f:
-                f.write(err_msg)
+    if "pythonw" in sys.executable:
+        err_msg = "Error : pyexewrap should never be running with pythonw.exe !\n" + str(sys.executable) + "\n" + str(sys.argv)
+        print(err_msg)
+        with open("error.txt", encoding="UTF-8") as f:
+            f.write(err_msg)
 
-        try:
-            ################ INITIALIZATION ##############
-            if pyexewrap_verbose: 
-                print("interpreter is " + sys.executable)
-                print("CLI is " + " ".join(sys.argv))
-                print("script extension is " + script_extension)
-                print("script_is_doubleclicked=" + str(script_is_doubleclicked))
+    try:
+        ################ INITIALIZATION ##############
+        if pyexewrap_verbose: 
+            print("interpreter is " + sys.executable)
+            print("CLI is " + " ".join(sys.argv))
+            print("script extension is " + script_extension)
+            print("script_is_doubleclicked=" + str(script_is_doubleclicked))
+        
+        # .pyw files should have a hidden console unless an exception occurs
+        if script_extension == ".pyw":
+            User32.show_window(User32.Const.SW_HIDE)  # Use SW_SHOWMINIMIZED to debug
+        
+        # if not run in console (but with py.exe through double-click) the window title will be explicit
+        if script_is_doubleclicked and pyexewrap_must_change_title:
+            os.system("title " + os.path.basename(script_to_execute) + " -- pyexewrap " + script_to_execute)
+        
+        with open(script_to_execute) as f:
+            script_code = f.read()
+
+        ################ CODE INJECTION ###############
+        # I have deprecated the use of code injection since it was creating a shift in the line numbering of the
+        # exception tracebacks. One idea could be to inject code only on the first line but it is equivalent to
+        # executing a code here first and then use exec().
+        # Code _modification_ coulds still be a thing though. For replacing a variable by another or changing paths, etc.
             
-            # .pyw files should have a hidden console unless an exception occurs
-            if script_extension == ".pyw":
-                User32.show_window(User32.Const.SW_HIDE)  # Use SW_SHOWMINIMIZED to debug
-            
-            # if not run in console (but with py.exe through double-click) the window title will be explicit
-            if script_is_doubleclicked and pyexewrap_must_change_title:
-                os.system("title " + os.path.basename(script_to_execute) + " -- pyexewrap " + script_to_execute)
-            
-            with open(script_to_execute) as f:
-                script_code = f.read()
+        ################ COMPILATION AND EXECUTION ####
+        #https://docs.python.org/3/library/functions.html?highlight=exec#exec
 
-            ################ CODE INJECTION ###############
-            # I have deprecated the use of code injection since it was creating a shift in the line numbering of the
-            # exception tracebacks. One idea could be to inject code only on the first line but it is equivalent to
-            # executing a code here first and then use exec().
-            # Code _modification_ coulds still be a thing though. For replacing a variable by another or changing paths, etc.
-                
-            ################ COMPILATION AND EXECUTION ####
-            #https://docs.python.org/3/library/functions.html?highlight=exec#exec
-            # We don't want the namespace of the exec(compiled_code) being polluted by the imports and declarations of pyexewrap
-            # One option was to make a copy of globals() and remove unwanted imports but I am affraid this would eventually be forgotten for future imports
-            # The other option is to cherry-pick the only necessary global variables. This may have side effects as well since new 
-            # mandatory global vars may appear in python in a distant future.
-            # note that '__file__' key is not set to __file__ but to script_to_execute (see unitary test E004)
-            # note that we could identify the list of default global vars of a python execution from unitary test E004 
-            globalsParameter = {'__annotations__':__annotations__,
-                                '__builtins__' : __builtins__,
-                                '__cached__':None,
-                                '__doc__':__doc__,
-                                '__file__':script_to_execute,
-                                '__loader__':__loader__,
-                                '__name__':__name__,
-                                '__package__':None,
-                                '__spec__':__spec__}
-            localsParameter = {'pyexewrap_must_pause_in_console': pyexewrap_must_pause_in_console}
-            compiled_code = compile(script_code, script_to_execute, "exec")
-            exec(compiled_code, globalsParameter, localsParameter)
-            # I couldn't figure out why the for loop won't work :
-            # for key in localsParameter.keys(): locals()[key] = localsParameter[key]
-            # I had to explicitly retreive the local variable
-            pyexewrap_must_pause_in_console = localsParameter['pyexewrap_must_pause_in_console']
-                
+        ## I HAD TO GIVE UP ON THIS DUE TO E001 (import statements not importing modules properly)
+        ## # We don't want the namespace of the exec(compiled_code) being polluted by the imports and declarations of pyexewrap
+        ## # One option was to make a copy of globals() and remove unwanted imports but I am affraid this would eventually be forgotten for future imports
+        ## # The other option is to cherry-pick the only necessary global variables. This may have side effects as well since new 
+        ## # mandatory global vars may appear in python in a distant future.
+        ## # note that '__file__' key is not set to __file__ but to script_to_execute (see unitary test E004)
+        ## # note that we could identify the list of default global vars of a python execution from unitary test E004 
+        ## globalsParameter = {'__annotations__':__annotations__,
+        ##                     '__builtins__' : __builtins__,
+        ##                     '__cached__':None,
+        ##                     '__doc__':__doc__,
+        ##                     '__file__':script_to_execute,
+        ##                     '__loader__':__loader__,
+        ##                     '__name__':__name__,
+        ##                     '__package__':None,
+        ##                     '__spec__':__spec__}
+        ## localsParameter = {'pyexewrap_must_pause_in_console': pyexewrap_must_pause_in_console}
+        ## compiled_code = compile(script_code, script_to_execute, "exec")
+        ## exec(compiled_code, globalsParameter, globalsParameter|localsParameter)
 
-            if pyexewrap_verbose: print("pyexewrap_must_pause_in_console="+str(pyexewrap_must_pause_in_console))
+        # Too bad I have to do it this way (see above):
+        globals()['__file__'] = script_to_execute
+        compiled_code = compile(script_code, script_to_execute, "exec")
+        exec(compiled_code, globals(), globals())
 
-        except BaseException as e:
-            if script_extension == ".pyw":
-                # From now on pyexewrap will consider the script as a .py file (with a pausing message to display)
-                script_extension = ".py"
-                User32.show_window(User32.Const.SW_SHOWDEFAULT)
-            # These 2 lines are now replaced by showtraceback():
-            # print(f"Error executing {script_to_execute}: {type(e).__name__}")
-            # print(traceback.format_exc())
-            showtraceback()
-            print("This exception has ended the script before the end.")
+        # pp.pprint(
+
+        ## # I couldn't figure out why the for loop won't work :
+        ## # for key in localsParameter.keys(): locals()[key] = localsParameter[key]
+        ## # I had to explicitly retreive the local variable
+        ## pyexewrap_must_pause_in_console = localsParameter['pyexewrap_must_pause_in_console']
+         
+
+        if pyexewrap_verbose: print("pyexewrap_must_pause_in_console="+str(pyexewrap_must_pause_in_console))
+
+    except BaseException as e:
+        pyexewrap_must_pause_in_console = True
+        if script_extension == ".pyw":
+            # From now on pyexewrap will consider the script as a .py file (with a pausing message to display)
+            script_extension = ".py"
+            User32.show_window(User32.Const.SW_SHOWDEFAULT)
+        # These 2 lines are now replaced by showtraceback():
+        # print(f"Error executing {script_to_execute}: {type(e).__name__}")
+        # print(traceback.format_exc())
+        showtraceback()
+        print("This exception has ended the script before the end.")
 
     pause_decision = script_is_doubleclicked and pyexewrap_must_pause_in_console and script_extension != ".pyw"
     if pyexewrap_verbose: 
@@ -193,13 +205,27 @@ def main():
         print("script_extension=" + script_extension)
         print("pause_decision=" + str(pause_decision))
 
-    # PAUSING MESSAGE AT THE END OF THE SCRIPT
-    if pause_decision:
-        display_pause_prompt_and_menu()
-    
-    if pyexewrap_verbose: print("pyexewrap ended.")
-    sys.exit()
+    return pause_decision
 
+# Main function of the pyexewrap package
+def main():
+    if len(sys.argv) < 2: 
+        print("Usage: python wrap.py <script.py>")
+        return
+    
+    # Looping since the script can be run multiple times
+    must_run_script_again = True
+    while must_run_script_again:
+        
+        # But in most cases the script is only run once
+        must_run_script_again = False
+        
+        # The script is run, depending on the situation there should be a pausing prompt
+        pause_decision = run_script(sys.argv[1])
+        
+        # Diplaying the pausing prompt (and defining if the script must be run again)
+        if pause_decision:
+            must_run_script_again = display_pause_prompt_and_menu()
 
 if __name__ == "__main__":
     main()
